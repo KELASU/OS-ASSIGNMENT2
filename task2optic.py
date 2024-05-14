3f652aaae12e7bbdc86ebadfff0257674eb0f8e7
KELASU <95676450+KELASU@users.noreply.github.com>
2024-05-14 21:06:14 +0700

    cgefef

--- task2optic.py
+++ task2optic.py
@@ -25,18 +25,38 @@ def scan(requests, initial_position):
     requests = deque(sorted(requests, key=lambda x: x.cylinder))
     head_movements = 0
     current_position = initial_position
-
-    while requests:
-        next_request = next((r for r in requests if r.cylinder >= current_position), None)
-        if next_request:
-            head_movements += next_request.cylinder - current_position
-            current_position = next_request.cylinder
-            requests.remove(next_request)
-        else:
-            head_movements += 4999 - current_position
-            current_position = 0
-
+    direction = 1
+    while requests or (current_position + direction < 4999 and direction == 1) or (current_position - direction > 0 and direction == -1):
+        if requests and requests[0].cylinder == current_position:
+            requests.popleft()
+        if not requests and current_position < 4999 and direction == 1:
+            current_position += 1
+            continue
+        if not requests and current_position > 0 and direction == -1:
+            current_position -= 1
+            continue
+        if current_position < 4999 and direction == 1:
+            next_request = next((r for r in requests if r.cylinder >= current_position), None)
+            if next_request:
+                head_movements += next_request.cylinder - current_position
+                current_position = next_request.cylinder
+                requests.popleft()
+            else:
+                head_movements += 4999 - current_position
+                current_position = 4999
+                direction = -1
+        elif current_position > 0 and direction == -1:
+            next_request = next((r for r in requests if r.cylinder <= current_position), None)
+            if next_request:
+                head_movements += current_position - next_request.cylinder
+                current_position = next_request.cylinder
+                requests.popleft()
+            else:
+                head_movements += current_position
+                current_position = 0
+                direction = 1
     return head_movements
+
 def c_scan(requests, initial_position):
     requests = deque(sorted(requests, key=lambda x: x.cylinder))
     head_movements = 0
@@ -64,8 +84,6 @@ def main():
         initial_position = int(sys.argv[2])
 
     requests = read_requests(file_name)
-    
-
     print("FCFS:")
     fcfs_head_movements = fcfs(requests, initial_position)
     print(f"Total head movements: {fcfs_head_movements}")
